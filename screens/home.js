4bb35433c272a7ee329e311fdad2c79057504af7
RobCM <robert.coleman.martinez@gmail.com>
2021-09-08 17:04:41 -0500

    Create home.js

---
+++ screens/home.js
@@ -0,0 +1,15 @@
+/*
+    App home screen
+    @RobertC 9/7/2021
+
+*/
+import {Text, View} from 'react-native';
+import React from 'react';
+
+export default function HomeScreen(){
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Hello from Da Home Screen Bro!!!</Text>
+    </View>
+  );
+};
