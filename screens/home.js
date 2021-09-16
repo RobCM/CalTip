65e9b4fe7328de1adaa26888c97f3b34645e90bb
RobCM <robert.coleman.martinez@gmail.com>
2021-09-16 17:31:59 -0500

    Update home.js

--- screens/home.js
+++ screens/home.js
@@ -3,13 +3,18 @@
     @RobertC 9/7/2021
 
 */
-import {Text, View} from 'react-native';
+import {Text, View, Button, Alert} from 'react-native';
 import React from 'react';
 
 export default function HomeScreen(){
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Hello from Da Home Screen Bro!!!</Text>
+      <Button
+        title="CALCULATE"
+        color="#f194ff"
+        onPress={() => Alert.alert('Button pressed to calculate!')}
+      />
     </View>
   );
 };
