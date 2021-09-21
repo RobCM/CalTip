89819a2186862fc503680a18a212b3a913841fa1
RobCM <robert.coleman.martinez@gmail.com>
2021-09-21 17:03:30 -0500

    Update home.js

--- screens/home.js
+++ screens/home.js
@@ -3,18 +3,40 @@
     @RobertC 9/7/2021
 
 */
-import {Text, View, Button, Alert} from 'react-native';
+import {StyleSheet, Text, View, Button, Alert,  TextInput} from 'react-native';
 import React from 'react';
 
 export default function HomeScreen(){
+
+  const [text, onChangeText] = React.useState("Input Total Here");
+  const [number, onChangeNumber] = React.useState(null);
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+
       <Text>Hello from Da Home Screen Bro!!!</Text>
+
+      <TextInput
+       style={home_styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+
       <Button
         title="CALCULATE"
         color="#f194ff"
         onPress={() => Alert.alert('Button pressed to calculate!')}
       />
+
     </View>
   );
 };
+
+const home_styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
